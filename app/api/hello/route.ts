90134c99d617c337bb704b341b2f0cb55a5f03b8
SalahAmm <salahamm4year@gmail.com>
2026-04-28 21:48:27 +0100

    Learned Post Request

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,32 +1,48 @@
 import { NextRequest } from "next/server";
 
 
+const users: { id: number; name: string; email: string }[] = [];
+
+
+let nextId = 1;
+
+
 
 export async function GET(request: NextRequest) {
 
-    const searchParams = request.nextUrl.searchParams;
-    const query = searchParams.get("get");
-    const limit = searchParams.get("limit");
-
-
-    if (!query) {
-        return Response.json(
-            { error: "Missing 'q'  paramater" },
-            { status: 400 },
-
-        );
-    }
-
-
-    const result = [
-        { name: "John Doe", age: 25 },
-        { name: "Jane Smith", age: 30 },
-        { name: "Bob Johnson", age: 40 }
-    ].slice(0, Number(limit) || 10);
-  
-
-    return Response.json({ query, result });
+	return Response.json({users});
 };
+
+
+export async function POST( request : NextRequest) {
+
+
+	const body = await request.json();
+
+
+	if(!body.name || !body.email) {
+		return Response.json(
+			{error: "Eather a Name or Email is missing!!"},
+			{status: 400},
+		);
+	};
+
+
+
+const newUser = {
+	id: nextId,
+	name: body.name,
+	email: body.email,
+};
+
+
+users.push(newUser);
+
+
+
+return Response.json(newUser , {status : 201});
+
+}
 
 
 
